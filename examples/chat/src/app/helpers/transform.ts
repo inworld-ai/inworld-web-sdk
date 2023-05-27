0cce1f5fc4945628b358d35b7c588efa2e39b610
Tatyana Shashkova <tatyana.shashkova@inworld.ai>
2023-05-26 18:05:49 -0700

    feat: Add audio playback config to example

--- examples/chat/src/app/helpers/transform.ts
+++ examples/chat/src/app/helpers/transform.ts
@@ -1,2 +1,10 @@
 export const dateWithMilliseconds = (date: Date) =>
   `${date.toLocaleString()}.${date.getMilliseconds()}`;
+
+export const toInt = (value: string | number) => {
+  if (typeof value === 'number') return value;
+
+  const parsed = parseInt(value) ?? undefined;
+
+  return isNaN(parsed) ? undefined : parsed;
+};
