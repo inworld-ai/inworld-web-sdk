eff77b0ca7eed3f6a667dec33295033c7c0dc5b3
Peter Couture <disfractal@tornor.com>
2023-08-09 00:33:16 +0800

    Adding the locally hosted files to the chat .gitignore

--- examples/chat/src/config.ts
+++ examples/chat/src/config.ts
@@ -6,8 +6,7 @@ export const Config: Configuration = {
   DEFAULT_ANIMATION: 'Neutral_Hello_Long_01',
   GENERATE_TOKEN_URL:
     process.env.REACT_APP_GENERATE_TOKEN_URL || 'http://localhost:4000',
-  IMAGES_BODY_URI:
-    'https://storage.googleapis.com/assets-inworld-ai/models/innequin/textures/body/',
+  IMAGES_BODY_URI: './assets/textures/body/', // https://storage.googleapis.com/assets-inworld-ai/models/innequin/textures/body/
   IMAGES_FACIAL_URI:
     'https://storage.googleapis.com/assets-inworld-ai/models/innequin/imgs/face/',
   INWORLD_CHARACTER: process.env.REACT_APP_INWORLD_CHARACTER,
@@ -17,7 +16,7 @@ export const Config: Configuration = {
   CAMERA_SETTINGS: {
     POS_X: 0,
     POS_Y: 2,
-    POS_Z: 4,
+    POS_Z: 3,
     TAR_X: 0,
     TAR_Y: 1,
     TAR_Z: 0,
