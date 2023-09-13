365745103e22c82f4c4d52cf287ee69702ffeb97
Tatyana Shashkova <tatyana.shashkova@inworld.ai>
2023-09-12 17:50:07 -0700

    fix: Remove excess colors

--- examples/chat/src/app/configuration/SessionContinuation.tsx
+++ examples/chat/src/app/configuration/SessionContinuation.tsx
@@ -22,24 +22,17 @@ export const SessionContinuation = () => {
   const enabled = watch('continuation.enabled');
 
   const blue = {
-    100: '#DAECFF',
     200: '#b6daff',
     400: '#3399FF',
     500: '#007FFF',
-    600: '#0072E5',
-    900: '#003A75',
   };
 
   const grey = {
     50: '#f6f8fa',
-    100: '#eaeef2',
     200: '#d0d7de',
     300: '#afb8c1',
-    400: '#8c959f',
     500: '#6e7781',
-    600: '#57606a',
     700: '#424a53',
-    800: '#32383f',
     900: '#24292f',
   };
 
