0e95c7d9ba6395d42c2bcad8da000d3666f2479f
Peter Couture <disfractal@tornor.com>
2024-04-24 23:59:57 +0800

    feat: finalized minification

--- packages/web-threejs/rollup.config.mjs
+++ packages/web-threejs/rollup.config.mjs
@@ -3,7 +3,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 export default {
   input: './build/src/index.js',
   output: {
-    file: './dist/inworld-web-core.min.js',
+    file: './dist/inworld-web-threejs.min.js',
     format: 'cjs',
   },
   plugins: [nodeResolve()],
